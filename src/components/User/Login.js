ba11ae0861cd34552031d796749a206be2de6a61
Badshah2507 <54072406+Badshah2507@users.noreply.github.com>
2023-09-22 11:14:05 +0530

    Update Login.js

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../../styles/Login.css'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Col, Row, InputGroup } from 'react-bootstrap'
 import loginLogo from './../../images/Login.png'
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
@@ -8,47 +8,56 @@ import { SERVER_URL } from '../../config';
 import { Url } from '../../Url';
 
 export default function UserLogin() {
-
+    const [validated, setValidated] = useState(false);
     const [userName, setUserName] = useState("");
     const [userPwd, setUserPwd] = useState("");
     const [alert, setAlert] = useState(false);
     const { login } = useAuth();
     const navigate = useNavigate();
 
-    const onSubmitLogin = async () => {
-        await fetch(SERVER_URL+Url.LOGIN,{
-            method: 'POST',
-            body: JSON.stringify({
-                name: userName,
-                password: userPwd
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-             },
-        })
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            setUserName("")
-            setUserPwd("")
-            setAlert(true)
-            console.log("Login Failed");
-        })
-        .then((data) => {
-            if(data.role === "User") {
-                login(data);
-                navigate("/user/home", {replace: true})
-            } else {
+    const onSubmitLogin = async (e) => {
+        const form = e.currentTarget;
+        if (form.checkValidity() === true) {
+            console.log(1)
+            await fetch(SERVER_URL+Url.LOGIN,{
+                method: 'POST',
+                body: JSON.stringify({
+                    name: userName,
+                    password: userPwd
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
                 setUserName("")
                 setUserPwd("")
                 setAlert(true)
                 console.log("Login Failed");
-            }
-        })
-        .catch((err) => {
-            console.log(err)
-        }) 
+            })
+            .then((data) => {
+                if(data.role === "User") {
+                    login(data);
+                    navigate("/user/home", {replace: true})
+                } else {
+                    setUserName("")
+                    setUserPwd("")
+                    setAlert(true)
+                    console.log("Login Failed");
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            }) 
+        } else {
+            console.log(2)
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        setValidated(true);
     }
 
     return (
@@ -60,13 +69,21 @@ export default function UserLogin() {
             <div className='login-box'>
                 <h2 className='mb-3'>User Login</h2>
                 <div style={{ width: "70%" }}>
+                    <Form noValidate validated={validated} onClick={onSubmitLogin}>
+                    {/* <Form.Group md="3"> */}
                     <Form.Label>Username</Form.Label>
                     <Form.Control
                         id="userID"
                         className='mb-3'
                         value={userName}
                         onChange={(e) => setUserName(e.target.value)}
+                        required
                     />
+                    <Form.Control.Feedback type="invalid">
+                        Please enter a username.
+                    </Form.Control.Feedback>
+                    {/* </Form.Group> */}
+                    {/* <Form.Group md="3"> */}
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                         type="password"
@@ -74,15 +91,21 @@ export default function UserLogin() {
                         className='mb-4'
                         value={userPwd}
                         onChange={(e) => (setUserPwd(e.target.value))}
+                        required
                     />
+                    <Form.Control.Feedback type="invalid">
+                        Please enter a password.
+                    </Form.Control.Feedback>
+                    {/* </Form.Group> */}
                     { alert ? 
                         <div className="alert alert-danger" role="alert">
                             Username or Password is Incorrect!!!
                         </div> : <></>
                     }
-                    <Button variant="primary" type="submit" style={{ width: "100%" }} size="lg" className='mb-3' onClick={onSubmitLogin}>
+                    <Button variant="primary" type="submit" style={{ width: "100%" }} size="lg" className='mb-3'>
                         Submit
                     </Button>
+                    </Form>
                     <div style={{ textAlign: "center" }}>
                         <span>Admin Login Link: </span><a href="/admin/login">Here</a>
                     </div>
